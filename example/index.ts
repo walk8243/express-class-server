2512804ab3409ba241cfe18a83c0b14811af06f1
walk8243 <walk8243@gmail.com>
2020-04-09 19:28:36 +0900

    サンプルを更新

--- example/index.ts
+++ example/index.ts
@@ -2,11 +2,18 @@ import { App } from '../';
 
 const app = new App([
 	{
+		path: '/',
 		handler: (req, res, next) => {
-			setTimeout(() => {
-				res.send({ result: 'express-class-server' });
-			}, 5 * 1000);
+			res.send({ result: 'express-class-server' });
 		},
 	},
+	{
+		path: '/delay',
+		handler: (req, res, next) => {
+			setTimeout(() => {
+				res.send({ result: 'express-class-server', type: 'delay' });
+			}, 5 * 1000);
+		}
+	}
 ]);
 app.start();
